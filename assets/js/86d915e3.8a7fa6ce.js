4fdb007e1c5b08903843f3f7353606df7e6311f2
postyizhan <97342038+postyizhan@users.noreply.github.com>
2024-04-26 12:48:54 +0000

    Deploying to gh-pages from @ postyizhan/NitWikit@be0ad52fea2ae94cc33b3f4c743d1a06d44727f6 🚀

---
+++ assets/js/86d915e3.8a7fa6ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkWiki=self.webpackChunkWiki||[]).push([[9762],{1594:e=>{e.exports=JSON.parse('{"title":"npc","slug":"/category/npc","permalink":"/category/npc","navigation":{"previous":{"title":"\u6982\u89c8","permalink":"/\u63d2\u4ef6/TAB&\u8bb0\u5206\u677f/\u6982\u89c8"},"next":{"title":"citizens","permalink":"/\u63d2\u4ef6/npc/citizens"}}}')}}]);
